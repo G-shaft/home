5d180a9d95101e11aaa4e1ec37aff93dec0ae017
JohnShaft <61475374+G-shaft@users.noreply.github.com>
2020-03-10 21:23:55 +0400

    Create homeBot.py

---
+++ homeBot.py
@@ -0,0 +1,27 @@
+import telebot
+
+#from telebot import apihelper
+API_TOKEN = '1111494654:AAFGfe1jLP5A1JD79hSeM-97y4wVbzToFGo'
+
+bot = telebot.TeleBot(API_TOKEN)
+user = bot.get_me()
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_message(message):
+    bot.reply_to(message, "hello")
+
+@bot.message_handler(commands=['photo'])
+def send_welcome(message):
+    proc = subprocess.Popen('./test.sh', stdout=subprocess.PIPE)
+    output = proc.stdout.read()
+    photo = open('1.jpg', 'rb')
+    bot.send_photo(message.chat.id, photo)
+    
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+#879159087
+bot.polling()
