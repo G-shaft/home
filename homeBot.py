fe097975b991cc1cf649ce11c3119299bc83d558
JohnShaft <61475374+G-shaft@users.noreply.github.com>
2020-03-10 21:24:50 +0400

    Update homeBot.py

--- homeBot.py
+++ homeBot.py
@@ -13,10 +13,12 @@ def send_message(message):
 
 @bot.message_handler(commands=['photo'])
 def send_welcome(message):
-    proc = subprocess.Popen('./test.sh', stdout=subprocess.PIPE)
+    proc = subprocess.Popen('./crphoto.sh', stdout=subprocess.PIPE)
     output = proc.stdout.read()
     photo = open('1.jpg', 'rb')
     bot.send_photo(message.chat.id, photo)
+    proc = subprocess.Popen('./dphoto.sh', stdout=subprocess.PIPE)
+    output = proc.stdout.read()
     
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
